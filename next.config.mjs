7cefd357a4dc941d84b0ccd5f7cc8a0964398d0e
JINSEBAK <sejisesang@gmail.com>
2025-09-02 11:00:17 +0900

    chore: github pages 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export",
+  basePath: isProd ? "/refresh" : "",
+  assetPrefix: isProd ? "/refresh/" : "",
+  images: {
+    unoptimized: true
+  }
+};
 
 export default nextConfig;
