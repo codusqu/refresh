4abc1193d7297fb2d761af978c6a952ebf2f7ba8
JINSEBAK <sejisesang@gmail.com>
2025-09-02 11:30:18 +0900

    feat: 첫페이지 생성

---
+++ src/app/about/page.js
@@ -0,0 +1,14 @@
+import styles from "../page.module.css";
+
+const AboutContainer = () => {
+  return (
+    <div className={styles.content}>
+      <button type="button" className={styles.btn}>
+        뒤로가기
+      </button>
+      <section></section>
+    </div>
+  );
+};
+
+export default AboutContainer;
